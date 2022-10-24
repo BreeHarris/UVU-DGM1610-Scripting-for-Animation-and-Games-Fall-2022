0e85c59f2f6aa39c4534d69daf6472d7ac01c5a3
Bree Harris <10890584@uvu.edu>
2022-10-24 15:17:06 -0600

    Assets and Score Manager

---
+++ 2D Platformer/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score; // Keep our score value
+    public TextMeshProUGUI scoreText; // Reference visual text UI element to change
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //scoreText = GetComponent<TextMeshProUGUI>
+    }
+    //This function rewards the player
+
+    public void IncreaseScore(int amount)
+    {
+        score += amount; //add amount to the score
+        UpdateScoreText(); //Update the UI text
+    }
+   
+
+
+    public void UpdateScoreText()
+    {
+        scoreText.text = "Score: " + score;
+
+    }
+}
